2ab5490780953c79add3a971db85e08a3656f6c8
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-06-12 12:11:06 +0000

    Unit5-02 PHP

---
+++ results.php
@@ -0,0 +1,22 @@
+<?php
+    // Setting the variables
+    $userNumFirst = $_POST['firstNumber'];
+    $userNumSecond = $_POST['secondNumber'];
+
+    // initalize numbers as an empty string
+    $sum = 0;
+    $numbers = "";
+
+    for ($counter = 0; $counter < $userNumSecond; $countr++) {
+        if ($counter < $userNumSecond){
+        $numbers = $numbers . $userNumFirst . "+";
+    }
+    else {
+        $numbers = $numbers + $counter;
+    }
+    $sum = $sum + $userNumFirst;
+}
+?>	
+	<!-- display the results -->
+    <h4>Results:</h4>
+    <?php echo $numbers + " = " + $sum ?>
